f7248f2b6892ac227d41e20caabdb7564d590c9a
aamirhassan547 <81614752+aamirhassan547@users.noreply.github.com>
2025-08-28 21:30:26 +0100

    Update urls.py

--- videos/urls.py
+++ videos/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('video/<int:video_id>/like/', views.like_video, name='like_video'),
     path('api/videos/', views.video_list_api, name='video_list_api'),
     path('api/videos/<int:video_id>/', views.video_detail_api, name='video_detail_api'),
+    path('get-azure-sas-token/', views.get_azure_sas_token, name='get_azure_sas_token'),
+
 ]
