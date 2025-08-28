30c45ac17c8aa0f7bccbaf5f19305ef45112baff
aamirhassan547 <81614752+aamirhassan547@users.noreply.github.com>
2025-08-28 19:33:01 +0100

    Update settings.py

--- snapshare/settings.py
+++ snapshare/settings.py
@@ -1,4 +1,4 @@
-﻿import os
+import os
 from pathlib import Path
 from dotenv import load_dotenv
 from decouple import config
@@ -9,9 +9,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-default-key-for-dev')
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'True') == 'False'
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = [
+    'snapshareapp-emcdepcadfcmgzcg.uksouth-01.azurewebsites.net',
+    'localhost',
+    '127.0.0.1'
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://snapshareapp-emcdepcadfcmgzcg.uksouth-01.azurewebsites.net',
+    'http://localhost:8000',
+    'http://127.0.0.1:8000'
+]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -61,14 +71,13 @@ WSGI_APPLICATION = 'snapshare.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME', default='snapshare'),
-        'USER': config('DB_USER', default='postgres'),
-        'PASSWORD': config('DB_PASSWORD', default='abcdefghi'),
-        'HOST': config('DB_HOST', default='localhost'),
+        'NAME': config('DB_NAME', default='postgres'),
+        'USER': config('DB_USER', default='hassan'),
+        'PASSWORD': config('DB_PASSWORD', default='Swordfish1122'),
+        'HOST': config('DB_HOST', default='snapsharedb.postgres.database.azure.com'),
         'PORT': config('DB_PORT', default='5432'),
     }
 }
-
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -90,11 +99,25 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
+# Static files configuration
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# Azure Blob Storage Configuration for media files
+DEFAULT_FILE_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+AZURE_ACCOUNT_NAME = config('AZURE_ACCOUNT_NAME', default='snapsharemedia')
+AZURE_ACCOUNT_KEY = config('AZURE_ACCOUNT_KEY', default='11oVcZSxnX0xMdZzwYvUZQhYVVZlzjwHSf90FPmIJJl+UGG7tecQuhTjtI3sP1eKODdz/T504i6c+AStTx7+ig==')
+AZURE_CONTAINER = config('AZURE_CONTAINER', default='media')
+AZURE_CUSTOM_DOMAIN = f'{AZURE_ACCOUNT_NAME}.blob.core.windows.net'
+AZURE_URL_EXPIRATION_SECS = 3600  # URL expiration time in seconds (optional)
+
+# Media files will be served from Azure Blob Storage
+MEDIA_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_CONTAINER}/'
+
+# Optional: If you want to use Azure for static files as well, uncomment below
+# STATICFILES_STORAGE = 'storages.backends.azure_storage.AzureStorage'
+# AZURE_STATIC_CONTAINER = config('AZURE_STATIC_CONTAINER', default='static')
+# STATIC_URL = f'https://{AZURE_CUSTOM_DOMAIN}/{AZURE_STATIC_CONTAINER}/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
